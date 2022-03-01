35b1f2997f5b7ec0da9287a26ec010dc3d17f8b2
Akshay Bodla <akshaybodla21@gmail.com>
2022-03-01 10:28:40 -0600

    starting implementation of alias

--- mysh.c
+++ mysh.c
@@ -4,6 +4,17 @@
 #include <string.h>
 
 void batchMode();
+void runAlias(char *cmd);
+
+/*
+* Linked list/dictionary implementation of aliases
+* The key is the shortcut while value is the actual command
+*/
+struct aliasLinkedList {
+    char* key;
+    char* value;
+    struct aliasLinkedList *next; //if there's no other alias then this is null
+};
 
 char* prompt="mysh> \0";
 
@@ -14,21 +25,38 @@ int main(int argc, char* argv[]) {
 
     //else run interactive mode
     char *s = malloc(512);
+    //char *eof = malloc(4);
+
+    //TODO: Handle breaking when user presses ctrl+d
     while(1) {
 
+        /* prompt user to enter & immediately exit if exit typed */
         write(1, prompt, sizeof(prompt));
         fgets(s, 512, stdin);
+        //eof = fgets(s, 512, stdin);
 
         //write(1, s, sizeof(s));
         if(!strncmp(s, "exit", 4))
             break;
 
+        /* run appropriate program based on input */
+        if(!strncmp(s, "alias", 4))
+            runAlias(s);
+
+
     }
 
 
     free(s);
+    //free(eof);
 }
 
 void batchMode() {
     write(5, "batch", 5);
+}
+
+void runAlias(char *cmd) {
+
+
+    return;
 }
