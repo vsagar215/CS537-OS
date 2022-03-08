177af27ae37c854d41f8408fadb65c214b89723a
Akshay Bodla <akshaybodla21@gmail.com>
2022-03-08 09:33:28 -0600

    Last night's everything

--- p2b/mysh.c
+++ p2b/mysh.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 /*
  * Linked list/dictionary implementation of aliases
@@ -16,29 +17,28 @@ struct aliasLinkedList
 };
 const int BUFFER_SIZE = 512;
 const int TOKEN_NUMBER = 256;
-char *prompt = "mysh> \0";
+// char *prompt = "mysh> ";
 
 // Function signatures
 void batchMode(char *fileName);
 void readCmd(FILE *stream);
 void execCmd(char *cmd);
 char **tokenizeCmd(char *cmd);
-int parseInput(char *tokens[TOKEN_NUMBER], char *cmd);                                                       // returns number of tokens
-int checkRedirection(char *cmd);
+int parseInput(char *tokens[TOKEN_NUMBER], char *cmd); // returns number of tokens
+int checkRedirection(char *cmd, char *redirPtr);
+void execRedir(char **old, char **new, char *fileName);
+char *getFn(char *cmd);
 struct aliasLinkedList *runAlias(struct aliasLinkedList *head, char *tokens[TOKEN_NUMBER], int numTokens);
 void runUnalias(struct aliasLinkedList **head, char *cmd[], int numTokens);
 void addNode(struct aliasLinkedList **headRef, char *key, char *value);
 void removeNode(struct aliasLinkedList **headRef, char *key);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // buffer for write
     char *buff;
     int buff_size;
-
     // Incorrect number of cmd line args
-    if (argc > 2)
-    {
+    if (argc > 2) {
         buff = "Usage: mysh [batch-file]\n";
         buff_size = strlen(buff);
         write(STDERR_FILENO, buff, buff_size);
@@ -58,24 +58,24 @@ int main(int argc, char *argv[])
     char *cmd = malloc(BUFFER_SIZE);
 
     // TODO: Handle exiting when user presses ctrl+d
-    while (1)
-    {
+    while (1) {
         /* prompt user to enter & immediately exit if exit typed */
-        write(1, prompt, sizeof(prompt));
+        printf("mysh> ");
+        fflush(stdout);
         fgets(cmd, BUFFER_SIZE, stdin);
 
         char *tokens[TOKEN_NUMBER];
         int numTokens = parseInput(tokens, cmd);
 
         // terminate shell if user types exit
-        if (!strncmp(cmd, "exit", 4))
+        if (!strncmp(cmd, "exit\n", 4))
             exit(0);
 
         /* run appropriate program based on input */
-        //check if cmd is an alias
+        // check if cmd is an alias
         findAlias = head;
-        while(findAlias != NULL) {
-            if(!strncmp(findAlias->key, tokens[0], sizeof(*(findAlias->key)) )) {
+        while (findAlias != NULL) {
+            if (!strncmp(findAlias->key, tokens[0], sizeof(*(findAlias->key)))) {
                 /*Build the actual command*/
                 execCmd(findAlias->value);
             }
@@ -83,19 +83,17 @@ int main(int argc, char *argv[])
         }
 
         // Feature: Alias and Unalias
-        if(!strncmp(cmd, "alias", 4))
+        if (!strncmp(cmd, "alias", 4))
             head = runAlias(head, tokens, numTokens);
-        else if(!strncmp(cmd, "unalias", 7))
+        else if (!strncmp(cmd, "unalias", 7))
             runUnalias(&head, tokens, numTokens);
         else
             execCmd(cmd);
-        //else if(!strcmp(tokens[0], "/bin/ls\n") || !strncmp(tokens[0],"/bin/ls", sizeof("/bin/ls")))
-            //execCmd(cmd);
     }
 
-    //Free the linked list
+    // Free the linked list
     struct aliasLinkedList *curr = head, *prev = NULL;
-    while(curr != NULL){
+    while (curr != NULL) {
         prev = curr;
         curr = curr->next;
         free(prev);
@@ -105,22 +103,15 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void batchMode(char *fileName)
-{
-    // buffer for write
-    char *buff;
-    int buff_size;
-
+void batchMode(char *fileName) {
     // Opening file passed as cmdline arg
     FILE *fp = fopen(fileName, "r");
 
     // Checking if file opened successfully
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         // Batch-file DNE
-        buff = "Error: Cannot open file <filename>\n";
-        buff_size = strlen(buff);
-        write(STDERR_FILENO, buff, buff_size);
+        fprintf(stderr, "Error: Cannot open file %s.\n", fileName);
+        fflush(stderr);
         exit(1);
     }
 
@@ -131,66 +122,78 @@ void batchMode(char *fileName)
 }
 
 // Prints line with appropirate prefix string
-void readCmd(FILE *stream)
-{
+// Prints line with appropirate prefix string
+void readCmd(FILE *stream) {
     char s[BUFFER_SIZE];
-    while (fgets(s, BUFFER_SIZE, stream) != NULL)
-    {
-        if (strcmp(s, "exit") == 0)
+    while (fgets(s, BUFFER_SIZE, stream) != NULL) {
+        printf("%s", s);
+        fflush(stdout);
+
+        if (strcmp(s, "exit") == 0 || strcmp(s, "exit\n") == 0)
             exit(0);
-        //printf("%s", s);
         execCmd(s);
     }
     // End of file reached
     exit(0);
 }
-
-void execCmd(char *cmd)
-{
+void execCmd(char *cmd) {
     // populating executCmd with cmd
     char **executCmd;
     executCmd = tokenizeCmd(cmd);
+    char *fileName = malloc(0);
+    int toRedir = checkRedirection(cmd, fileName);
+    if(toRedir == 0)
+        fileName = getFn(cmd);
+    if (toRedir < 0)
+        return;
+    // Creating
+    int num;
 
-    // // Printing full command
-    // char *tok = executCmd[0];
-    // int i = 0;
-    // while (tok != NULL)
-    // {
-    //     printf("%s\t", tok);
-    //     tok = executCmd[++i];
-    // }
+    while(strcmp(executCmd[num], ">") != 0){
+        num++;
+    }
 
+    //char **newCmd = malloc(sizeof(char *) * num +1);
+
+    char *buff = strtok(cmd, ">");
+
+    //printf("pointer: %p\n", newCmd);
+    printf("buff: %s\n", buff);
+    //while(buff != NULL){
+    //    newCmd[num] = strdup(buff)
+    //}
+    //
     int PID = fork();
     int status;
     // Checking if fork fails
-    if (PID == -1)
-    {
+    if (PID == -1) {
+        free(fileName);
         exit(1);
     }
     // Fork succeeds:
     // 1) Child Process
-    if (PID != 0)
-    {
+    int execRV;
+    if (PID == 0) {
         // This is the child process
         // exec does not return if it succeeds
         // returns (and sets errno) -1 if fails
+        if (toRedir == 0) {
+            char **badExecutCmd = tokenizeCmd(cmd);
+            execRedir(badExecutCmd, executCmd, fileName);
+            char *xyz[] ={"/bin/ls",NULL};
+            execRV = execv(executCmd[0], xyz);
+        }
+        else execRV = execv(executCmd[0], executCmd);
 
-        int isCmdRedirected = checkRedirection(cmd);
-
-        if(isCmdRedirected == 0){
-            int execRV = execv(executCmd[0], executCmd);
-            if (execRV == -1)
-            {
-                printf("%s: Command not found.\n", executCmd[0]);
-                fflush(stdout);
-                // child process terminates itself
-                _exit(1);
-            }
+        if (execRV == -1) {
+            printf("%s: Command not found.\n", executCmd[0]);
+            fflush(stdout);
+            // child process terminates itself
+            _exit(1);
         }
     }
     // 2) Parents Process
-    else
-    {
+    else {
         // This is the parent process; Waits for the child to finish
         waitpid(PID, &status, 0);
         free(executCmd);
@@ -198,55 +201,46 @@ void execCmd(char *cmd)
 }
 
 // TODO: Use parseInput to compute duplicate?
-char **tokenizeCmd(char *cmd)
-{
+char **tokenizeCmd(char *cmd) {
+    int dumCount = 0, tokCount = 0;
     // terminating in \n
     cmd[strcspn(cmd, "\n")] = 0;
-    int tokenCount = 0;
-    // int otherC = 0; // Counts mismatch -- look into this
-    // char *tokens[TOKEN_NUMBER];
-    // otherC = parseInput(tokens, cmd);
-    // printf("%p\n", tokens);
-    char *dup = strdup(cmd);        // creating duplicate string
-    char *token = strtok(cmd, " "); // splitting on spaces
+    char *dummy = strdup(cmd);
+    char *dummy2 = strdup(cmd);
+    // splitting on spaces
+    char *token = strtok(dummy, " ");
 
     // counting tokens
-    while (token != NULL)
-    {
+    while (token != NULL) {
         token = strtok(NULL, " ");
-        tokenCount += 1;
+        dumCount++;
     }
 
-    char *tokenDup = strtok(dup, " ");
-    char **myargv = malloc(sizeof(char *) * tokenCount + 1);
-    tokenCount = 0;
+    // tokenized pointer to string
+    char **tokenized = malloc(sizeof(char *) * dumCount + 1);
+    char *tokBuf = strtok(dummy2, " ");
 
-    // populate myargv array
-    while (tokenDup != NULL)
-    {
-        myargv[tokenCount] = strdup(tokenDup); // TODO: free all pointers of argv
-        tokenDup = strtok(NULL, " ");
-        tokenCount += 1;
+    // Populating tokenized array
+    while (tokBuf != NULL) {
+        tokenized[tokCount] = strdup(tokBuf);
+        tokBuf = strtok(NULL, " ");
+        tokCount++;
     }
 
     // append null terminator to end of myargv
-    myargv[tokenCount] = NULL;
-    free(dup);
-
-    // fprintf(stdout, "real: %d\tother: %d", tokenCount, otherC);
-    return myargv;
+    tokenized[tokCount] = NULL;
+    free(dummy);
+    free(dummy2);
+    return tokenized;
 }
 
-
-int parseInput(char *tokens[TOKEN_NUMBER], char *cmd)
-{
+int parseInput(char *tokens[TOKEN_NUMBER], char *cmd) {
     // tokenize cmd
     // TODO: maybe duplicate the cmd to preserve it
     char *tempCmd = strdup(cmd);
     char *token = strtok(tempCmd, " ");
     int currToken = 0;
-    do
-    {
+    do {
         tokens[currToken] = token;
         currToken++;
         token = strtok(NULL, " "); // manuals specify this must be null
@@ -255,32 +249,71 @@ int parseInput(char *tokens[TOKEN_NUMBER], char *cmd)
     return currToken;
 }
 
-int checkRedirection(char *cmd) {
-    char *pch = strchr(str,'>');
-    int numRedirChar = 0;
-    //int pos = pch-str+1; //honestly not sure what this does, found it in the manuals and it works…
-    while (pch!=NULL) {
-        printf ("found at %d\n",pch-str+1);
-        pch=strchr(pch+1,'>');
-    }
-
-    if(numRedirChar == 0 || numRedirChar > 1) {
-        write(1, "Redirection misformatted.\n", sizeof("Redirection misformatted.\n"));
-        return 0;
-    }
-
-
-
-    return pos; //ptr to pos of ">"
+char *getFn(char *cmd) {
+    char **tokCmd = tokenizeCmd(cmd);
+    int i = 0;
+    while (tokCmd[i] != NULL)
+        ++i;
+    char* redirPtr = strdup(tokCmd[i-1]); // the file to open
+    return redirPtr;
 }
 
-struct aliasLinkedList* runAlias(struct aliasLinkedList *head, char *tokens[256], int numTokens) {
+int checkRedirection(char *cmd, char *redirPtr) {
+    if (cmd == NULL)
+        return -1;
 
-    //printf("testing\n");
-    //fflush(stdout);
+    if (strchr(cmd, '>') == NULL)
+        return 1;
+
+    char **tokCmd = tokenizeCmd(cmd);
+    int redirCount = 0;
+    char last = cmd[strlen(cmd) - 1], first = cmd[0];
+    if (last == '>' || first == '>') {
+        write(1, "Redirection misformatted.\n", sizeof("Redirection misformatted.\n"));
+        return -1;
+    }
+
+    int i = 0;
+    while (tokCmd[i] != NULL)
+        ++i;
+
+    if (!strcmp(tokCmd[i - 1], ">")) {
+        write(1, "Redirection misformatted.\n", sizeof("Redirection misformatted.\n"));
+        return -1;
+    }
+
+    redirPtr = strdup(tokCmd[i-1]); // the file to open
+    for (int j = 0; j < i; j++) {
+        if (!strcmp(tokCmd[j], ">"))
+            redirCount++;
+    }
+    if (redirCount > 1) {
+        write(1, "Redirection misformatted.\n", sizeof("Redirection misformatted.\n"));
+        return -1;
+    }
+    return 0;
+}
+
+void execRedir(char **old, char **new, char *fileName) {
+    // Closing stdout & Handling FileIO
+    int fptr = open(fileName, O_WRONLY | O_TRUNC);
+    dup2(fptr, STDOUT_FILENO);
+    if(fptr == -1) {
+        fprintf(stderr, "Cannot write to file %s.\n", fileName);
+        fflush(stderr);
+    }
+
+    //int i = 0;
+    //while(!strcmp(old[i], ">")) {
+        //printf("i:%d %s\n", i, old[i]);
+        //i++;
+    //}
+}
+
+struct aliasLinkedList *runAlias(struct aliasLinkedList *head, char *tokens[256], int numTokens) {
     struct aliasLinkedList *currentNode = head;
-    if(!strcmp(tokens[0], "alias\n")){
-        while(currentNode->next != NULL) {
+    if (!strcmp(tokens[0], "alias\n")) {
+        while (currentNode->next != NULL) {
             printf("%s %s", currentNode->key, currentNode->value);
             fflush(stdout);
             currentNode = currentNode->next;
@@ -289,19 +322,17 @@ struct aliasLinkedList* runAlias(struct aliasLinkedList *head, char *tokens[256]
     }
 
     /*If the user only types alias*/
-    if(!strncmp(tokens[1], "alias\n", sizeof("alias")) ||
+    if (!strncmp(tokens[1], "alias\n", sizeof("alias")) ||
         !strncmp(tokens[1], "unalias\n", sizeof("unalias")) ||
-        !strncmp(tokens[1], "exit\n", sizeof("exit"))
-        ){
+        !strncmp(tokens[1], "exit\n", sizeof("exit"))) {
         write(2, "alias: Too dangerous to alias that.\n", sizeof("alias: Too dangerous to alias that.\n"));
         return head;
     }
 
-
     /* if of form command alias <alias-name> then numTokens == 2, so then only print that one alias */
-    if(numTokens == 2) {
-        while(currentNode->next != NULL) {
-            if(!strncmp(currentNode->key, tokens[1], sizeof(*(currentNode->key)))) {
+    if (numTokens == 2) {
+        while (currentNode->next != NULL) {
+            if (!strncmp(currentNode->key, tokens[1], sizeof(*(currentNode->key)))) {
                 printf("%s %s", currentNode->key, currentNode->value);
                 fflush(stdout);
                 return head;
@@ -313,30 +344,30 @@ struct aliasLinkedList* runAlias(struct aliasLinkedList *head, char *tokens[256]
 
     /* build the value of the alias */
     char *aliasVal = "\0", *temp = malloc(sizeof(aliasVal));
-    if(temp == NULL){
+    if (temp == NULL) {
         write(2, "Error not enough memory\n", sizeof("Error not enough memory\n"));
         return head;
     }
 
-    for(int i = 2; i < numTokens; i++) {
-
+    for (int i = 2; i < numTokens; i++) {
         temp = realloc(temp, sizeof(aliasVal) + sizeof(tokens[i]));
-        if(temp == NULL){
+        if (temp == NULL) {
             write(2, "Error not enough memory\n", sizeof("Error not enough memory\n"));
             return head;
         }
         temp = strdup(aliasVal);
         strcat(temp, tokens[i]);
 
-        if(i < numTokens-1) strcat(temp, " ");
+        if (i < numTokens - 1)
+            strcat(temp, " ");
         aliasVal = strdup(temp);
     }
 
     /*
-    * Iterate through nodes until
-    * 1) Find a node with the current alias
-    * 2) Reach the end
-    */
+     * Iterate through nodes until
+     * 1) Find a node with the current alias
+     * 2) Reach the end
+     */
     addNode(&head, tokens[1], aliasVal);
     free(temp);
     return head;
@@ -344,34 +375,34 @@ struct aliasLinkedList* runAlias(struct aliasLinkedList *head, char *tokens[256]
 
 void addNode(struct aliasLinkedList **head, char *key, char *value) {
     struct aliasLinkedList *currentNode = *head;
-    if(head == NULL || *head == NULL) {
+    if (head == NULL || *head == NULL) {
         head = malloc(sizeof(struct aliasLinkedList));
-        (*head)->key   = strdup(key);
+        (*head)->key = strdup(key);
         (*head)->value = strdup(value);
-        (*head)->next  = NULL;
+        (*head)->next = NULL;
     }
 
-    while(currentNode->next != NULL ) {
-        if(!strcmp(currentNode->key, key)) break;
+    while (currentNode->next != NULL) {
+        if (!strcmp(currentNode->key, key))
+            break;
         currentNode = currentNode->next;
     }
 
-    if(!strcmp(currentNode->key, key))
+    if (!strcmp(currentNode->key, key))
         currentNode->value = strdup(value);
     else {
         struct aliasLinkedList *newNode = malloc(sizeof(struct aliasLinkedList));
-        newNode->key   = malloc(sizeof(key));
+        newNode->key = malloc(sizeof(key));
         newNode->value = malloc(sizeof(value));
-        newNode->key   = strdup(key);
+        newNode->key = strdup(key);
         newNode->value = strdup(value);
-        newNode->next  = *head;
+        newNode->next = *head;
         *head = newNode;
     }
 }
 
 void runUnalias(struct aliasLinkedList **headRef, char *cmd[], int numTokens) {
-
-    if(numTokens > 2) {
+    if (numTokens > 2) {
         write(1, "unalias: Incorrect number of arguments.\n", sizeof("unalias: Incorrect number of arguments.\n"));
         return;
     }
@@ -379,7 +410,7 @@ void runUnalias(struct aliasLinkedList **headRef, char *cmd[], int numTokens) {
     /*Edge Case: remove from head*/
     char *key = cmd[1];
     struct aliasLinkedList *head = *headRef;
-    if( !strncmp(head->key, key, sizeof( *(head->key) )) ) {
+    if (!strncmp(head->key, key, sizeof(*(head->key)))) {
         *headRef = (*headRef)->next;
         free(head);
         return;
@@ -387,16 +418,14 @@ void runUnalias(struct aliasLinkedList **headRef, char *cmd[], int numTokens) {
 
     /*Remove elsewhere*/
     struct aliasLinkedList *curr = head, *prev = head;
-    while(curr != NULL) {
-
-        if(!strncmp(curr->key, key, sizeof(*(curr->key)) )) {
+    while (curr != NULL) {
+        if (!strncmp(curr->key, key, sizeof(*(curr->key)))) {
             prev->next = curr->next;
             free(curr->key);
             free(curr->value);
             free(curr);
             return;
         }
-
         prev = curr;
         curr = curr->next;
     }
